15d3a170ca7dfe0c2f3795722810045437b3c067
Aaditya Chauhan <aaditya.chauhan21052007@gmail.com>
2026-02-26 21:31:02 +0530

    Fixed particle Overlapping Issue and enhanced the collision...

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 #define width 800
 #define  height 600
-static const int size1=10;
+static const int size1=5;
 class particle{
     public:
     float x,y,radius,vx,vy;
@@ -17,7 +17,7 @@ random_device rd;
 for(int i=0; i<size1; i++){
     
     
-    particles[i].radius=random_radius(gen);
+    particles[i].radius=50;
     uniform_int_distribution<> random_width(particles[i].radius,width-particles[i].radius);
     uniform_int_distribution<> random_height(particles[i].radius,height-particles[i].radius);
     particles[i].x=random_width(gen);
@@ -48,33 +48,44 @@ void updateparticle(particle *particles){
     particles[i].x+=particles[i].vx;
     particles[i].y+=particles[i].vy;
 }
+} 
+void particleoverlapping(particle *particles, int i, int j){
+        float x=particles[j].x-particles[i].x;
+         float y=particles[j].y-particles[i].y;
+         float distance = sqrt((x*x)+(y*y));
+         float overlap = (particles[i].radius+particles[j].radius)-distance;
+         if(overlap>0){
+x/=distance;
+y/=distance;
+particles[i].x-=x*overlap/2;
+particles[i].y-=y*overlap/2;
+particles[j].x+=x*overlap/2;
+particles[j].y+=y*overlap/2;
+         }
+         if(overlap==0) return;
 }
-void particlecollison(particle *particles){
+void particlecollision(particle *particles){
     for(int i=0; i<size1; i++){
         for(int j=0; j<size1; j++){
             if(i==j) continue;
-            Vector2 c1={particles[i].x, particles[i].y};
+               Vector2 c1={particles[i].x, particles[i].y};
             Vector2 c2={particles[j].x, particles[j].y};
-            bool collide = CheckCollisionCircles(c1,particles[i].radius,c2,particles[j].radius);
-            
+             bool collide = CheckCollisionCircles(c1,particles[i].radius,c2,particles[j].radius);
             if(collide){
-                particles[i].x-=20;
-                particles[i].y-=20;
-                particles[j].x+=20;
-                particles[j].y+=20;
-                 particles[i].vx*=-1;
-                 particles[i].vy*=-1;
-                 particles[j].vx*=-1;
-                 particles[j].vy*=-1;
-                 cout<<"Collision Occured";
+                cout<<"Collision Occured"<<endl;
+                
+                swap(particles[i].vx, particles[j].vx);
+    swap(particles[i].vy, particles[j].vy);
+    particleoverlapping(particles,i,j);
             }
         }
     }
 }
+
 int main(){
 particle particles[size1];
 InitWindow(width,height,"Particle Collision Simulator");
-SetTargetFPS(30);
+SetTargetFPS(60);
 setparticle(particles);
 while(!WindowShouldClose()){
     
@@ -82,7 +93,8 @@ BeginDrawing();
 ClearBackground(BLACK);
 drawparticle(particles);
 updateparticle(particles);
-particlecollison(particles);
+particlecollision(particles);
+
 DrawFPS(5,5);
 EndDrawing();
 }
